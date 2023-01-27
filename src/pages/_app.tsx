40f006b6cb11ade460bcd1a1f57645145cd14acd
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-01-27 17:01:51 +0100

    imported theme and requires font weights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '@/styles/globals.css';
+import '@fontsource/mulish/400.css';
+import '@fontsource/mulish/700.css';
+import '@fontsource/mulish/900.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
