4cfaf199ffdb6cad0b6efd153002684c4fe67896
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-02-13 17:28:56 +0100

    implemented Next Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,15 @@ import '@fontsource/mulish/700.css';
 import '@fontsource/mulish/900.css';
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import Layout from '../components/layout';
 import theme from './theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
