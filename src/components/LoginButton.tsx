01d1fbbf78ff19a03066ebb89c9377bb84e82a60
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-02-20 23:32:26 +0100

    created button component, to be fine tuned later

---
+++ src/components/LoginButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button, Link } from '@chakra-ui/react';
+
+const LoginButton = () => {
+  return (
+    <Button color="#FFFFFF" backgroundColor="#3A66DB" width="100%">
+      <Link href="https://github.com/emmanueloloke">Login</Link>
+    </Button>
+  );
+};
+
+export default LoginButton;
