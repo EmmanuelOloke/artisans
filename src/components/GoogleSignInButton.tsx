fad35f8cdba82e5b49012410fbb6f5512bd11262
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-22 14:26:04 +0100

    created google sign in button component

---
+++ src/components/GoogleSignInButton.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Button, Link } from '@chakra-ui/react';
+import { EmailIcon } from '@chakra-ui/icons';
+import { FaGoogle } from 'react-icons/fa';
+
+const LoginButton = () => {
+  return (
+    <Link
+      href="https://github.com/emmanueloloke"
+      textDecoration="none"
+      _hover={{ textDecoration: 'none' }}
+    >
+      <Button
+        leftIcon={<FaGoogle />}
+        color="gray"
+        border="1px solid #e7e7e9"
+        backgroundColor="white"
+        width="25rem"
+        fontWeight="bold"
+        _hover={{ backgroundColor: '#e7e7e9' }}
+      >
+        Sign in with Google
+      </Button>
+    </Link>
+  );
+};
+
+export default LoginButton;
