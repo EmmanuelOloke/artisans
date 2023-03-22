16565e7ef60c050847efaefb8103b185afa3246f
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-03-22 14:26:36 +0100

    style adjustments to login button

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -3,9 +3,21 @@ import { Button, Link } from '@chakra-ui/react';
 
 const LoginButton = () => {
   return (
-    <Button color="#FFFFFF" backgroundColor="#3A66DB" width="100%">
-      <Link href="https://github.com/emmanueloloke">Login</Link>
-    </Button>
+    <Link
+      href="https://github.com/emmanueloloke"
+      textDecoration="none"
+      _hover={{ textDecoration: 'none' }}
+    >
+      <Button
+        color="#FFFFFF"
+        backgroundColor="#3A66DB"
+        width="25rem"
+        fontWeight="bold"
+        _hover={{ backgroundColor: '#82A2F7' }}
+      >
+        Login
+      </Button>
+    </Link>
   );
 };
 
